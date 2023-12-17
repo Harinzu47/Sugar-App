e549113a74972975f17b5649661c45ca27612e91
Harinzu47 <harinzu47@gmail.com>
2023-12-17 22:02:06 +0700

    cors policy fix

--- src/scripts/global/api-endpoint.js
+++ src/scripts/global/api-endpoint.js
@@ -5,7 +5,7 @@ const API_ENDPOINT = {
   NORMALSUGAR: `${CONFIG.BASE_URL}recipes/findByNutrients?apiKey=${CONFIG.KEY}&maxCarbs=50&minSugar=10&maxSugar=50`,
   HIGHSUGAR: `${CONFIG.BASE_URL}recipes/findByNutrients?apiKey=${CONFIG.KEY}&maxCarbs=100&minSugar=50&maxSugar=100`,
   DETAIL: (id) => `${CONFIG.BASE_URL}recipes/${id}/information/?apiKey=${CONFIG.KEY}`,
-  BLOGS: `${CONFIG.BASE_URL2}everything?domains=medicalnewstoday.com&q=diabetes&apiKey=${CONFIG.KEY2}`,
+  BLOGS: `${CONFIG.PROXYURL}${CONFIG.BASE_URL2}everything?domains=medicalnewstoday.com&q=diabetes&apiKey=${CONFIG.KEY2}`,
 
 
 };
--- src/scripts/global/config.js
+++ src/scripts/global/config.js
@@ -2,10 +2,7 @@ const CONFIG = {
   KEY: '2a9aba83d32d4aaa89639e86e1e6dff5',
   BASE_URL: 'https://api.spoonacular.com/',
   BASE_IMG_URL: 'https://spoonacular.com/cdn/ingredients_100x100/',
-  CACHE_NAME: new Date().toISOString(),
-  DATABASE_NAME: 'restaurant-catalogue-database',
-  DATABASE_VERSION: 1,
-  OBJECT_STORE_NAME: 'resto',
+  PROXYURL: "https://cors-anywhere.herokuapp.com/",
 
   KEY2: '7d45850042204fd8ae01928291039e81',
   BASE_URL2: 'https://newsapi.org/v2/',
